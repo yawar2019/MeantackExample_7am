4a3c805c4a3901d53637117064cfb46fdeced7a4
Yawar2019 <yawarali17@gmail.com>
2025-12-19 08:56:14 +0530

    committed Post Operation

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,36 @@
 import express  from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from './models/product.model.js';
 const app=express();
+//dotenv.config();
+app.use(express.json());
+
+
+const createProduct=async(req,res)=>
+{
+const product=req.body;
+if(!product.name|| !product.price)
+{
+    return res.status(400).json({success:false,message:"Please fill up all Fields"})
+}
+
+const newProduct=new Product(product);
+try{
+newProduct.save();
+    return res.status(201).json({success:true,data:newProduct});
+}
+catch(error)
+{
+console.log('error in create product'+error.message);
+return res.status(500).json({success:false,message:'Internal Server Error'});
+}
+
+}
+
+app.post('/api/products',createProduct)
 
 app.listen(5000,()=>{
-
+connectDB();
     console.log('Server Started')
 })
