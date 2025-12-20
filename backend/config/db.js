9615584d7366ccb7d4a15e17558a55ceef9a95a4
Yawar2019 <yawarali17@gmail.com>
2025-12-20 09:05:59 +0530

    committed all api code

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ export const connectDB=async()=>{
     try{
        // MONGO_URI='mongodb+srv://yawarali17_db_user:Du0WF2zacY0ImS8w@cluster0.g7asgvk.mongodb.net/products?appName=Cluster0';
 //const conn=await mongoose.connect(process.env.MONGO_URI);
-const conn=await mongoose.connect('mongodb+srv://yawarali17_db_user:Du0WF2zacY0ImS8w@cluster0.g7asgvk.mongodb.net/product?appName=Cluster0');
+const conn=await mongoose.connect('mongodb+srv://yawarali17_db_user:xxx@cluster0.g7asgvk.mongodb.net/product?appName=Cluster0');
 console.log(`mongoo connected':${conn.connection.host}`);
     }
     catch(error)
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,20 @@ const app=express();
 app.use(express.json());
 
 
+
+const getproducts=async (req,res)=>{
+try
+{
+const products=await Product.find({});
+ return res.status(200).json({success:true,data:products});
+}
+catch(error)
+{
+console.log('error in getting product'+error.message);
+return res.status(500).json({success:false,message:'Internal Server Error'});
+}
+}
+
 const createProduct=async(req,res)=>
 {
 const product=req.body;
@@ -28,7 +42,45 @@ return res.status(500).json({success:false,message:'Internal Server Error'});
 
 }
 
-app.post('/api/products',createProduct)
+const updateProduct=async(req,res)=>
+{
+try 
+{
+ const {id}=req.params;
+const product=req.body;
+const resProduct=await Product.findByIdAndUpdate(id,product,{new:true})
+return res.status(200).json({status:"success",data:product});
+
+}
+catch(error)
+{
+console.log('error in updating product'+error.message);
+return res.status(500).json({success:false,message:'Internal Server Error'});
+}
+
+}
+
+const deleteProduct=async(req,res)=>
+{
+try 
+{
+ const {id}=req.params;
+await Product.findByIdAndDelete(id);
+return res.status(200).json({status:"success",message:'record deleted successfully'});
+
+}
+catch(error)
+{
+console.log('error in deleting product'+error.message);
+return res.status(500).json({success:false,message:'Internal Server Error'});
+}
+
+}
+
+app.get('/api/products',getproducts);
+app.post('/api/products',createProduct);
+app.put('/api/products/:id',updateProduct);
+app.delete('/api/products/:id',deleteProduct);
 
 app.listen(5000,()=>{
 connectDB();
