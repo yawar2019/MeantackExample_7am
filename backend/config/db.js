ae34eec2ffa0b1a46e5a42f05f3c0f55592bb487
Yawar2019 <yawarali17@gmail.com>
2025-12-22 09:02:42 +0530

    committed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB=async()=>{
 
     try{
-       // MONGO_URI='mongodb+srv://yawarali17_db_user:Du0WF2zacY0ImS8w@cluster0.g7asgvk.mongodb.net/products?appName=Cluster0';
+       // MONGO_URI='mongodb+srv://yawarali17_db_user:xx@cluster0.g7asgvk.mongodb.net/products?appName=Cluster0';
 //const conn=await mongoose.connect(process.env.MONGO_URI);
-const conn=await mongoose.connect('mongodb+srv://yawarali17_db_user:Du0WF2zacY0ImS8w@cluster0.g7asgvk.mongodb.net/product?appName=Cluster0');
+const conn=await mongoose.connect('mongodb+srv://yawarali17_db_user:xx@cluster0.g7asgvk.mongodb.net/product?appName=Cluster0');
 console.log(`mongoo connected':${conn.connection.host}`);
     }
     catch(error)
